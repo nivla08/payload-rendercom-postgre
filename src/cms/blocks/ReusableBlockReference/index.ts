ded2677751d424c33df2ac15afd6fa5c2c6285d6
nivla08 <alvinsaxor001@gmail.com>
2026-03-23 19:35:30 +0800

    refactor(cms): organize blocks into per-block folders with hook placeholders

--- src/cms/blocks/ReusableBlockReference/index.ts
+++ src/cms/blocks/ReusableBlockReference/index.ts
@@ -1,6 +1,6 @@
 import type { Block } from 'payload'
 
-import { REUSABLE_BLOCK_REFERENCE_SLUG, SHARED_BLOCKS_COLLECTION_SLUG } from './contracts'
+import { REUSABLE_BLOCK_REFERENCE_SLUG, SHARED_BLOCKS_COLLECTION_SLUG } from '../contracts'
 
 export const ReusableBlockReferenceBlock: Block = {
   slug: REUSABLE_BLOCK_REFERENCE_SLUG,
